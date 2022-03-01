ccdfcdea6d0872c5bdeba81559fde30c812e65a1
Jyun <poex0716@gmail.com>
2022-03-01 23:09:33 +0800

    button css adjusting, sec1 background migrate to img

--- src/components/Sec1.js
+++ src/components/Sec1.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import bg03 from '../images/bg03.png';
 import bg04 from '../images/bg04.png';
 class Sec1 extends Component {
 
@@ -7,7 +8,12 @@ class Sec1 extends Component {
     
     return (
       <React.StrictMode>
-        <div className={`App ${style['bg03']}`}>
+        <div style={{
+          width : '100%',
+          height: '960px',
+          marginTop : '-8vh'
+        }}>
+          <img src={bg03} className={style['bg03']}/>
           <img src={bg04} className={style['bg04']}/>
 
         </div>
