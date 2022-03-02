43f132bd9d3e7917d77eaa68b27dc9bdc838c006
DESKTOP-KUNFAI7\petez <pete7611work@gmail.com>
2022-03-02 21:51:26 +0800

    sec3 footer first commit

---
+++ src/components/Sec3.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+export default class Sec3 extends Component {
+   
+    render() {
+        let { titles } = this.props;
+        return (
+            <footer className={`sec3-footer`}>
+             
+                <div className='row'>
+                    <div className='col-sm-5 sec3-footer-text DFKai'>
+                        <h3 className='lead'><strong>{titles.footer_lead1}</strong></h3>
+                        <p>{titles.footer_content1}</p>
+                        <h3 className='lead'><strong>{titles.footer_lead2}</strong></h3>
+                        <p>{titles.footer_content2}</p>
+                        <p>{titles.footer_content3}</p>
+                        <p>{titles.footer_content4}</p>
+                        <p>{titles.footer_content5}</p>
+                        <h3 className='lead'><strong>{titles.footer_lead3}</strong></h3>
+                        <p>{titles.footer_content6}</p>
+
+                    </div>
+                </div>
+                
+            </footer>
+        );
+    }
+}
