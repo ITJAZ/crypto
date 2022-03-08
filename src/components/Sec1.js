9f02fcf25e60ca25a9fd00701b3bbd2128959365
jyun <jyunan1995@gmail.com>
2022-03-08 23:34:20 +0800

    add logo1, 2; change css into scss

--- src/components/Sec1.js
+++ src/components/Sec1.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import bg04 from '../images/bg04.png';
+import logo1 from '../images/logo01.png';
+import logo2 from '../images/logo02.png';
 class Sec1 extends Component {
 
   render() {
@@ -11,6 +13,8 @@ class Sec1 extends Component {
           marginTop : '-8vh'
         }}>
           <img src={bg04} className={`bg04`}/>
+          <img src={logo1} className={`logo logo1`}/>
+          <img src={logo2} className={`logo logo2`}/>
 
         </div>
       </React.StrictMode>
