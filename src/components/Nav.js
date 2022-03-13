16d4ae858f0439780a60b7c91c41395db3ca12d3
jyun <jyunan1995@gmail.com>
2022-03-13 15:56:15 +0800

    scss &: usage on

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,21 +3,28 @@ import React, { Component } from "react";
 import WalletTool from './../wallet/walletTool';
 class Nav extends Component {
 
+  constructor(){
+    super();
+
+    this.ref = React.createRef();
+  }
+
   render() {
-    let { titles } = this.props;    
+    let { titles } = this.props;
     return (
       <nav
         className={`navbar navbar-expand-lg navbar-light sticky-top nav-custom`}
       >
         <img src={background1} className={`nav-bg`} />
         <div className="collapse navbar-collapse">
-          <ul className="navbar-nav mr-auto">
+          <ul className={`navbar-nav mr-auto`} ref={this.ref}>
+            <a className="navbar-toggler" onClick={this.toggleRwd.bind(this)}><span className="navbar-toggler-icon"></span></a>
             <li className="nav-item">
-              <a className={`btn custom-link`}>Cool Turtle</a>
+              <a className={`btn custom-nav-link`}>Cool Turtle</a>
             </li>
             <li className="nav-item">
               <a
-                className={`btn custom-link`}
+                className={`btn custom-nav-link`}
                 target="_blank"
                 href="https://discord.gg/DQJddvmn"
               >
@@ -26,7 +33,7 @@ class Nav extends Component {
             </li>
             <li className="nav-item">
               <a
-                className={`btn custom-link`}
+                className={`btn custom-nav-link`}
                 target="_blank"
                 href="https://twitter.com/CoolTurtle20xx"
               >
@@ -35,7 +42,7 @@ class Nav extends Component {
             </li>
             <li className="nav-item">
               <a
-                className={`btn custom-link`}
+                className={`btn custom-nav-link`}
                 target="_blank"
                 href="https://instagram.com/coolturtle.nft_?r=nametag"
               >
@@ -43,13 +50,13 @@ class Nav extends Component {
               </a>
             </li>
             <li className="nav-item">
-              <a className={`btn custom-link`}>OpenSea</a>
+              <a className={`btn custom-nav-link`}>OpenSea</a>
             </li>
             <li className="nav-item">
               {!WalletTool.isActive? 
-              <a key='connect' className={`btn custom-link`} onClick={WalletTool.connect}> Connect</a>
+              <a key='connect' className={`btn custom-nav-link`} onClick={WalletTool.connect}> Connect</a>
               :      
-              <a key='disconnect' className={`btn custom-link`} onClick={WalletTool.disConnect}> Disconnect</a>
+              <a key='disconnect' className={`btn custom-nav-link`} onClick={WalletTool.disConnect}> Disconnect</a>
               }
             </li>
             {WalletTool.isActive ? <span>Connecter:{WalletTool.getAccount}</span> : <span>no body</span>}
@@ -57,6 +64,11 @@ class Nav extends Component {
         </div>
       </nav>
     );
+  }
+
+  toggleRwd(){
+    let navbar = this.ref.current;
+    navbar.classList.toggle('responsive');
   }
 }
 export default Nav;
