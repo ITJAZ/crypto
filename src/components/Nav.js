c35e82240352a0013fec7153de48322f7478a8c5
jyun <jyunan1995@gmail.com>
2022-03-13 20:50:33 +0800

    nav RWD adjustment; showing but not working

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,27 @@
 import background1 from "../images/bg01.png";
 import React, { Component } from "react";
-import WalletTool from './../wallet/walletTool';
+import WalletTool from "./../wallet/walletTool";
 class Nav extends Component {
-
-  constructor(){
-    super();
-
-    this.ref = React.createRef();
-  }
-
   render() {
     let { titles } = this.props;
     return (
       <nav
         className={`navbar navbar-expand-lg navbar-light sticky-top nav-custom`}
       >
+        <button
+          type="button"
+          className="navbar-toggler"
+          data-toggle="collapse"
+          data-target="#customNavbarCollapse"
+          aria-controls="customNavbarCollapse"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <img src={background1} className={`nav-bg`} />
-        <div className="collapse navbar-collapse">
-          <ul className={`navbar-nav mr-auto`} ref={this.ref}>
-            <a className="navbar-toggler" onClick={this.toggleRwd.bind(this)}><span className="navbar-toggler-icon"></span></a>
+        <div id="customNavbarCollapse" className="collapse navbar-collapse">
+          <ul className={`navbar-nav mr-auto`}>
             <li className="nav-item">
               <a className={`btn custom-nav-link`}>Cool Turtle</a>
             </li>
@@ -53,22 +56,35 @@ class Nav extends Component {
               <a className={`btn custom-nav-link`}>OpenSea</a>
             </li>
             <li className="nav-item">
-              {!WalletTool.isActive? 
-              <a key='connect' className={`btn custom-nav-link`} onClick={WalletTool.connect}> Connect</a>
-              :      
-              <a key='disconnect' className={`btn custom-nav-link`} onClick={WalletTool.disConnect}> Disconnect</a>
-              }
+              {!WalletTool.isActive ? (
+                <a
+                  key="connect"
+                  className={`btn custom-nav-link`}
+                  onClick={WalletTool.connect}
+                >
+                  {" "}
+                  Connect
+                </a>
+              ) : (
+                <a
+                  key="disconnect"
+                  className={`btn custom-nav-link`}
+                  onClick={WalletTool.disConnect}
+                >
+                  {" "}
+                  Disconnect
+                </a>
+              )}
             </li>
-            {WalletTool.isActive ? <span>Connecter:{WalletTool.getAccount}</span> : <span>no body</span>}
+            {WalletTool.isActive ? (
+              <span>Connecter:{WalletTool.getAccount}</span>
+            ) : (
+              <span>no body</span>
+            )}
           </ul>
         </div>
       </nav>
     );
-  }
-
-  toggleRwd(){
-    let navbar = this.ref.current;
-    navbar.classList.toggle('responsive');
   }
 }
 export default Nav;
