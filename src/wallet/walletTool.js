6a57d0a3e4a90c328bd5c2dabd984ab5f39da257
tvpete <pete7611work@gmail.com>
2022-03-15 17:06:46 +0800

    1,using sweetAlert2 dialg 2,check browser has tool

--- src/wallet/walletTool.js
+++ src/wallet/walletTool.js
@@ -1,18 +1,28 @@
 import { useWeb3React } from "@web3-react/core";
 import {injected} from './connector';
+import showDialog from './../utils/showDialog'
 
-import {useConfirmationDialog} from './WarnLoseDialog'
 export default function WalletTool(){
     const {active, account, library, connector, activate, deactivate} = useWeb3React();
-    const { getConfirmation } = useConfirmationDialog();
+   
+    const missingTool = () => {
+      showDialog(
+        'error',
+        'MetaMask 元件尚未安裝?',
+        '瀏覽器必須安裝MetaMask才可以連結您的帳戶',
+        <a href="https://metamask.io/">
+          取得MetaMask元件
+        </a>,
+      );
+    };
+
     async function connect(){
       try{
-        if (typeof window.ethereum) {
-          await getConfirmation(true);
+        if (! typeof window.ethereum) {
+          missingTool();
         }else{
-          alert('y')
+          await activate(injected);
         }
-        await activate(injected);
       }catch(ex){
         console.log(ex);
       }
